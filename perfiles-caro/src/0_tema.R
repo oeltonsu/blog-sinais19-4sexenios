1191d26cba16514dfd361d3d7c4c23540fedf020
carowhitetower <torreblanca.carolina@gmail.com>
2020-11-10 17:36:39 -0500

    carpetita con mis scripts y graficas

---
+++ perfiles-caro/src/0_tema.R
@@ -0,0 +1,57 @@
+options(scipen=999)
+
+
+fill_base <-  c("#e71d36","#5f0f40","#8d99ae","#ff7d00","#DA666F","#FD7C6A")
+ fill_dos <-  c("#525252","#cccccc")
+
+
+ tema <-  theme_linedraw() +
+   theme(
+     text = element_text(color = "grey35", family = "Lato"),
+     plot.title = element_text(
+       size = 30,
+       face = "bold",
+       margin = margin(10, 1, 5, 1),
+       color = "black"
+     ),
+     plot.subtitle = element_text(
+       size = 22,
+       face = "bold",
+       color = "#666666",
+       margin = margin(1, 2, 5, 1)
+     ),
+     plot.caption = element_text(hjust = 0, size = 17),
+     panel.grid = element_line(linetype = 2),
+     legend.position = "top",
+     panel.grid.minor = element_blank(),
+     legend.title = element_text(
+       size = 18,
+       face = "bold",
+       family = "Lato Bold"
+     ),
+     legend.text = element_text(size = 18),
+     legend.title.align = 0.5,
+     axis.title = element_text(
+       size = 16,
+       hjust = .25,
+       face = "bold",
+       family = "Lato"
+     ),
+     axis.text = element_text(
+       size = 18,
+       face = "bold",
+       family = "Lato"
+     ),
+     strip.background = element_rect(fill = "#525252"),
+     strip.text.x = element_text(
+       size = 18,
+       face = "bold",
+       family = "Lato"
+     ),
+     strip.text.y = element_text(
+       size = 18,
+       face = "bold",
+       family = "Lato"
+     )
+   )
+
